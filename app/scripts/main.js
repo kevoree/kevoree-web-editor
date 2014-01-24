0038a6bd0944f805b53a01a8dd0dabb743558223
Maxime Tricoire <max.tricoire@gmail.com>
2014-01-24 18:07:34 +0100

    menu ui improved

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,16 +4,23 @@ var SaveModel = require('../../lib/command/ui/SaveModel');
 
 $(function () {
     var editor = new KevWebEditor();
-
-    Mousetrap.bind(['command+l', 'ctrl+l'], function () {
+    function loadModelCmd() {
         var cmd = new LoadModel();
         cmd.execute(editor);
         return false;
-    });
+    }
 
-    Mousetrap.bind(['command+s', 'ctrl+s'], function () {
+    function saveModelCmd() {
         var cmd = new SaveModel();
         cmd.execute(editor);
         return false;
-    });
+    }
+
+    // Menus links
+    $('#load').click(loadModelCmd);
+    $('#save-json').click(saveModelCmd);
+
+    // Keyboard shortcuts
+    Mousetrap.bind(['command+l', 'ctrl+l'], loadModelCmd);
+    Mousetrap.bind(['command+s', 'ctrl+s'], saveModelCmd);
 });
