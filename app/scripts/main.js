8f733b335f96c205e5fd6f691144fd7546342681
Maxime Tricoire <max.tricoire@gmail.com>
2014-01-24 17:50:49 +0100

    load & save ok

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,19 @@
 var KevWebEditor = require('../../lib/control/KevWebEditor');
 var LoadModel = require('../../lib/command/ui/LoadModel');
+var SaveModel = require('../../lib/command/ui/SaveModel');
 
 $(function () {
-  var editor = new KevWebEditor();
+    var editor = new KevWebEditor();
 
-  Mousetrap.bind(['command+l', 'ctrl+l'], function () {
-    var cmd = new LoadModel();
-    cmd.execute(editor);
-  });
+    Mousetrap.bind(['command+l', 'ctrl+l'], function () {
+        var cmd = new LoadModel();
+        cmd.execute(editor);
+        return false;
+    });
+
+    Mousetrap.bind(['command+s', 'ctrl+s'], function () {
+        var cmd = new SaveModel();
+        cmd.execute(editor);
+        return false;
+    });
 });
