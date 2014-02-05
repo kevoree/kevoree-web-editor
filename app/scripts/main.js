11ed91f6651e2706d60f8284af7a26116c308035
Maxime Tricoire <max.tricoire@gmail.com>
2014-02-05 16:20:14 +0100

    DA BIG COMMIT before checkout master

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,60 +1,64 @@
-var KevWebEditor = require('../../lib/control/KevWebEditor');
-var LoadModel    = require('../../lib/command/ui/LoadModel');
-var MergeModel    = require('../../lib/command/ui/MergeModel');
-var SaveModel    = require('../../lib/command/ui/SaveModel');
-var Settings     = require('../../lib/command/ui/Settings');
-var CloseModal   = require('../../lib/command/ui/CloseModal');
-var SaveModal    = require('../../lib/command/ui/SaveModal');
+var KevWebEditor = require('../../lib/engine/KevWebEditor'),
+    LoadModel    = require('../../lib/command/ui/LoadModel'),
+    MergeModel   = require('../../lib/command/ui/MergeModel'),
+    SaveModel    = require('../../lib/command/ui/SaveModel'),
+    Settings     = require('../../lib/command/ui/Settings'),
+    CloseModal   = require('../../lib/command/ui/CloseModal'),
+    Undo         = require('../../lib/command/Undo'),
+    Redo         = require('../../lib/command/Redo'),
+    SaveModal    = require('../../lib/command/ui/SaveModal');
 
 $(function () {
     var editor = new KevWebEditor();
 
-    function loadModelCmd(e) {
-        var cmd = new LoadModel();
-        cmd.execute(editor);
-        e.preventDefault();
-    }
-
-    function mergeModelCmd(e) {
-        var cmd = new MergeModel();
-        cmd.execute(editor);
-        e.preventDefault();
-    }
-
-    function saveModelCmd(e) {
-        var cmd = new SaveModel();
-        cmd.execute(editor);
-        e.preventDefault();
-    }
-
-    function openSettings(e) {
-        var cmd = new Settings();
-        cmd.execute();
-        e.preventDefault();
+    function executeCmd(Command, param) {
+        return function (e) {
+            var cmd = new Command(editor);
+            cmd.execute(param);
+            e.preventDefault();
+        }
     }
 
     // Menus links
-    $('#load').click(loadModelCmd);
-    $('#merge').click(mergeModelCmd);
-    $('#save-json').click(saveModelCmd);
-    $('#settings').click(openSettings);
+    $('#load').click(executeCmd(LoadModel));
+    $('#merge').click(executeCmd(MergeModel));
+    $('#save-json').click(executeCmd(SaveModel));
+    $('#settings').click(executeCmd(Settings));
+    $('#undo').click(executeCmd(Undo));
+    $('#redo').click(executeCmd(Redo));
 
     // Keyboard shortcuts
-    Mousetrap.bind(['command+l', 'ctrl+l'], loadModelCmd);
-    Mousetrap.bind(['command+m', 'ctrl+m'], mergeModelCmd);
-    Mousetrap.bind(['command+s', 'ctrl+s'], saveModelCmd);
-    Mousetrap.bind(['alt+s', 'alt+s'], openSettings);
-    Mousetrap.bind('escape', function (e) {
-        var cmd = new CloseModal();
-        cmd.execute();
-        e.preventDefault();
-    });
-    Mousetrap.bind('enter', function (e) {
-        var cmd = new SaveModal();
-        cmd.execute();
-        e.preventDefault();
-    });
+    Mousetrap.bind(['command+l', 'ctrl+l'], executeCmd(LoadModel));
+    Mousetrap.bind(['command+m', 'ctrl+m'], executeCmd(MergeModel));
+    Mousetrap.bind(['command+s', 'ctrl+s'], executeCmd(SaveModel));
+    Mousetrap.bind(['command+z', 'ctrl+z'], executeCmd(Undo));
+    Mousetrap.bind(['command+y', 'ctrl+y'], executeCmd(Redo));
+    Mousetrap.bind(['alt+s', 'alt+s'],      executeCmd(Settings));
+    Mousetrap.bind('escape',                executeCmd(CloseModal));
+    Mousetrap.bind('enter',                 executeCmd(SaveModal));
     Mousetrap.bind('up up down down left right left right b a enter', function() {
         console.log("Yeah, you mate know your classics!");
+    });
+
+    // Also allow model to be loaded from drag'n'drop
+    var domEditor = $('#editor');
+    domEditor.on('dragover', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+    domEditor.on('drop', function (e) {
+        if(e.originalEvent.dataTransfer){
+            if(e.originalEvent.dataTransfer.files.length) {
+                e.preventDefault();
+                e.stopPropagation();
+                /*UPLOAD FILES HERE*/
+                var file = e.originalEvent.dataTransfer.files[0],
+                    reader = new FileReader();
+                reader.onloadend = function(event) {
+                    executeCmd(LoadModel, event.target.result)(e);
+                };
+                reader.readAsText(file);
+            }
+        }
     });
 });
