bf92d8e5aa3971227e9d7eb8617f3d369f759204
Maxime Tricoire <max.tricoire@gmail.com>
2015-09-07 11:53:33 +0200

    retain user data for group push/pull

--- app/scripts/app/main/instance/group-push.controller.js
+++ app/scripts/app/main/instance/group-push.controller.js
@@ -1,105 +1,122 @@
 'use strict';
 
 angular.module('editorApp')
-    .controller('InstanceGroupPushCtrl', function ($scope, $modalInstance, $timeout, group, kEditor, kWs) {
-        $scope.type = 'push to';
-        $scope.action = 'push';
-        $scope.group = group;
-        $scope.selectedHost = '127.0.0.1';
-        $scope.selectedPort = '9000';
-        $scope.selectedPath = '/';
-        $scope.processing = false;
+  .factory('instanceGroupPushHolder', function() {
+    return {
+      host: '127.0.0.1',
+      port: '9000',
+      path: '/'
+    };
+  })
+  .controller('InstanceGroupPushCtrl', function($scope, $modalInstance, $timeout, group, kEditor, kWs, instanceGroupPushHolder) {
+    $scope.type = 'push to';
+    $scope.action = 'push';
+    $scope.group = group;
+    $scope.selectedHost = instanceGroupPushHolder.host;
+    $scope.selectedPort = instanceGroupPushHolder.port;
+    $scope.selectedPath = instanceGroupPushHolder.path;
+    $scope.processing = false;
 
-        $scope.hosts = {
-            '127.0.0.1': 'default'
-        };
+    $scope.hosts = {
+      '127.0.0.1': 'default'
+    };
 
-        $scope.ports = {
-            '9000': 'default'
-        };
+    $scope.ports = {
+      '9000': 'default'
+    };
 
-        $scope.paths = {
-            '/': 'default'
-        };
+    $scope.paths = {
+      '/': 'default'
+    };
 
-        if (group.dictionary) {
-            var host = group.dictionary.findValuesByID('host');
-            if (host && !$scope.hosts[host.value]) {
-                $scope.hosts[host.value] = group.name;
-            }
+    $scope.changeHost = function() {
+      instanceGroupPushHolder.host = $scope.selectedHost;
+    };
+    $scope.changePort = function() {
+      instanceGroupPushHolder.port = $scope.selectedPort;
+    };
+    $scope.changePath = function() {
+      instanceGroupPushHolder.path = $scope.selectedPath;
+    };
 
-            var port = group.dictionary.findValuesByID('port');
-            if (port && !$scope.ports[port.value]) {
-                $scope.ports[port.value] = group.name;
-            }
+    if (group.dictionary) {
+      var host = group.dictionary.findValuesByID('host');
+      if (host && !$scope.hosts[host.value]) {
+        $scope.hosts[host.value] = group.name;
+      }
 
-            var path = group.dictionary.findValuesByID('path');
-            if (path && !$scope.paths[path.value]) {
-                $scope.paths[path.value] = group.name;
-            }
+      var port = group.dictionary.findValuesByID('port');
+      if (port && !$scope.ports[port.value]) {
+        $scope.ports[port.value] = group.name;
+      }
+
+      var path = group.dictionary.findValuesByID('path');
+      if (path && !$scope.paths[path.value]) {
+        $scope.paths[path.value] = group.name;
+      }
+    }
+
+    group.subNodes.array.forEach(function(node) {
+      var fragDic = group.findFragmentDictionaryByID(node.name);
+
+      if (fragDic) {
+        var port = fragDic.findValuesByID('port'),
+          path = fragDic.findValuesByID('path');
+
+        if (port && !$scope.ports[port.value]) {
+          $scope.ports[port.value] = node.name;
         }
 
-        group.subNodes.array.forEach(function (node) {
-            var fragDic = group.findFragmentDictionaryByID(node.name);
+        if (path && !$scope.paths[path.value]) {
+          $scope.paths[path.value] = node.name;
+        }
+      }
 
-            if (fragDic) {
-                var port = fragDic.findValuesByID('port'),
-                    path = fragDic.findValuesByID('path');
-
-                if (port && !$scope.ports[port.value]) {
-                    $scope.ports[port.value] = node.name;
-                }
-
-                if (path && !$scope.paths[path.value]) {
-                    $scope.paths[path.value] = node.name;
-                }
-            }
-
-            node.networkInformation.array.forEach(function (net) {
-                net.values.array.forEach(function (val) {
-                    if (!$scope.hosts[val.value]) {
-                        $scope.hosts[val.value] = node.name;
-                    }
-                });
-            });
+      node.networkInformation.array.forEach(function(net) {
+        net.values.array.forEach(function(val) {
+          if (!$scope.hosts[val.value]) {
+            $scope.hosts[val.value] = node.name;
+          }
         });
-
-        $scope.closeError = function () {
-            $scope.error = null;
-        };
-
-        $scope.closeSuccess = function () {
-            $scope.success = null;
-        };
-
-        var ws;
-        $scope.confirm = function () {
-            $scope.error = null;
-            $scope.processing = true;
-            $timeout(function () {
-                ws = kWs.pushModel(
-                    kEditor.getModel(),
-                    $scope.selectedHost,
-                    $scope.selectedPort,
-                    $scope.selectedPath,
-                    function (err) {
-                        $timeout(function () {
-                            if (err) {
-                                $scope.processing = false;
-                                $scope.error = err.message;
-                            } else {
-                                $scope.processing = false;
-                                $scope.success = true;
-                            }
-                        });
-                    });
-            });
-        };
-
-        $scope.close = function () {
-            if (ws) {
-                ws.close();
-            }
-            $modalInstance.close();
-        };
+      });
     });
+
+    $scope.closeError = function() {
+      $scope.error = null;
+    };
+
+    $scope.closeSuccess = function() {
+      $scope.success = null;
+    };
+
+    var ws;
+    $scope.confirm = function() {
+      $scope.error = null;
+      $scope.processing = true;
+      $timeout(function() {
+        ws = kWs.pushModel(
+          kEditor.getModel(),
+          $scope.selectedHost,
+          $scope.selectedPort,
+          $scope.selectedPath,
+          function(err) {
+            $timeout(function() {
+              if (err) {
+                $scope.processing = false;
+                $scope.error = err.message;
+              } else {
+                $scope.processing = false;
+                $scope.success = true;
+              }
+            });
+          });
+      });
+    };
+
+    $scope.close = function() {
+      if (ws) {
+        ws.close();
+      }
+      $modalInstance.close();
+    };
+  });
