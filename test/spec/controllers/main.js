ebdee5c8cf7813da57f832739abd47f27aec0739
Maxime Tricoire <max.tricoire@gmail.com>
2015-09-16 14:51:31 +0200

    update tests and travis

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,7 +3,7 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('kevoreeWebEditorApp'));
+  beforeEach(module('editorApp'));
 
   var MainCtrl,
     scope;
@@ -15,8 +15,4 @@ describe('Controller: MainCtrl', function () {
       $scope: scope
     });
   }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
 });
