7015ee0c37dea3efb37f112b0579a4f0e886c872
Maxime Tricoire <max.tricoire@gmail.com>
2016-02-24 15:40:21 +0100

    v5.11.1: using timestamp to update registry libraries

--- app/scripts/app/libraries/libraries.controller.js
+++ app/scripts/app/libraries/libraries.controller.js
@@ -8,100 +8,101 @@
  * Controller of the editorApp registry libraries page
  */
 angular.module('editorApp')
-    .controller('LibrariesCtrl', function ($scope, $timeout, kRegistry, kModelHelper, kFactory, kEditor) {
-        $scope.loading = true;
+	.controller('LibrariesCtrl', function($scope, $timeout, kRegistry, kModelHelper, kFactory, kEditor) {
+		$scope.loading = true;
 
-        kRegistry
-            .init()
-            .then(function (tdefs) {
-                $scope.tdefs = tdefs;
-                $timeout(function () {
-                    angular.element('#filter-by-tdef').focus();
-                });
-            })
-            .catch(function (err) {
-                $scope.error = err.message;
-            })
-            .finally(function () {
-                $scope.loading = false;
-            });
+		kRegistry
+			.init()
+			.then(function(tdefs) {
+				console.log(tdefs);
+				$scope.tdefs = tdefs;
+				$timeout(function() {
+					angular.element('#filter-by-tdef').focus();
+				});
+			})
+			.catch(function(err) {
+				$scope.error = err.message;
+			})
+			.finally(function() {
+				$scope.loading = false;
+			});
 
-        $scope.select = function (evt, tdef) {
-            evt.preventDefault();
+		$scope.select = function(evt, tdef) {
+			evt.preventDefault();
 
-            if (!evt.ctrlKey) {
-                $scope.tdefs.getAll().forEach(function (item) {
-                    item.selected = false;
-                });
-            }
+			if (!evt.ctrlKey) {
+				$scope.tdefs.getAll().forEach(function(item) {
+					item.selected = false;
+				});
+			}
 
-            tdef.selected = !tdef.selected;
-            tdef.uiOpen = tdef.selected;
-        };
+			tdef.selected = !tdef.selected;
+			tdef.uiOpen = tdef.selected;
+		};
 
-        $scope.changeVersion = function () {
-            $scope.closeError();
-        };
+		$scope.changeVersion = function() {
+			$scope.closeError();
+		};
 
-        $scope.merge = function (tdef) {
-            kRegistry.get(kModelHelper.fqnToPath(tdef.package)+'/typeDefinitions[name='+tdef.name+',version='+tdef.version+']')
-                .then(function (tdefModel) {
-                    try {
-                        kEditor.merge(tdefModel);
-                        $scope.success = true;
-                    } catch (err) {
-                        $scope.error = err.message;
-                    }
-                })
-                .catch(function (err) {
-                    $scope.error = err.message;
-                });
-        };
+		$scope.merge = function(tdef) {
+			kRegistry.get(kModelHelper.fqnToPath(tdef.package) + '/typeDefinitions[name=' + tdef.name + ',version=' + tdef.version + ']')
+				.then(function(tdefModel) {
+					try {
+						kEditor.merge(tdefModel);
+						$scope.success = true;
+					} catch (err) {
+						$scope.error = err.message;
+					}
+				})
+				.catch(function(err) {
+					$scope.error = err.message;
+				});
+		};
 
-        $scope.mergeAll = function () {
-            $scope.tdefs.getAll()
-                .forEach(function (item) {
-                    if (item.selected && item.version) {
-                        $scope.merge(item);
-                    }
-                });
-        };
+		$scope.mergeAll = function() {
+			$scope.tdefs.getAll()
+				.forEach(function(item) {
+					if (item.selected && item.version) {
+						$scope.merge(item);
+					}
+				});
+		};
 
-        $scope.isMergeable = function (tdef) {
-            var mergeable = false;
+		$scope.isMergeable = function(tdef) {
+			var mergeable = false;
 
-            if (tdef.version) {
-                mergeable = !kEditor.getModel().findByPath(kModelHelper.pkgFqnToPath(tdef.package)+'/typeDefinitions[name='+tdef.name+',version='+tdef.version+']');
-            }
+			if (tdef.version) {
+				mergeable = !kEditor.getModel().findByPath(kModelHelper.pkgFqnToPath(tdef.package) + '/typeDefinitions[name=' + tdef.name + ',version=' + tdef.version + ']');
+			}
 
-            return mergeable;
-        };
+			return mergeable;
+		};
 
-        $scope.areMergeable = function () {
-            var tdefs = $scope.tdefs.getAll();
+		$scope.areMergeable = function() {
+			var tdefs = $scope.tdefs.getAll();
 
-            for (var i=0; i < tdefs.length; i++) {
-                if (tdefs[i].selected && tdefs[i].version) {
-                    if ($scope.isMergeable(tdefs[i])) {
-                        return true;
-                    }
-                }
-            }
+			for (var i = 0; i < tdefs.length; i++) {
+				if (tdefs[i].selected && tdefs[i].version) {
+					if ($scope.isMergeable(tdefs[i])) {
+						return true;
+					}
+				}
+			}
 
-            return false;
-        };
+			return false;
+		};
 
-        $scope.closeError = function () {
-            $scope.error = null;
-        };
+		$scope.closeError = function() {
+			$scope.error = null;
+		};
 
-        function beforeUnload() {
-            kRegistry.save();
-        }
+		function beforeUnload() {
+			kRegistry.save();
+		}
 
-        window.addEventListener('beforeunload', beforeUnload);
+		window.addEventListener('beforeunload', beforeUnload);
 
-        $scope.$on('$destroy', function () {
-            window.removeEventListener('beforeunload', beforeUnload);
-        });
-    });
+		$scope.$on('$destroy', function() {
+			window.removeEventListener('beforeunload', beforeUnload);
+		});
+	});
--- app/scripts/components/kevoree/registry.factory.js
+++ app/scripts/components/kevoree/registry.factory.js
@@ -1,226 +1,236 @@
 'use strict';
 
 angular.module('editorApp')
-    .factory('kRegistry', function($http, $q, storage, kFactory, kModelHelper, Notification, KEVOREE_REGISTRY_URL) {
-        var KEY_REG_LIBS = 'registry_libraries',
-            KEY_REG_MODEL = 'registry_model',
-            KEY_REG = 'registry';
-        var model, tdefs, initiated, takeFromStorage, listener;
-        var url = storage.get(KEY_REG, KEVOREE_REGISTRY_URL);
+	.factory('kRegistry', function($http, $q, storage, kFactory, kModelHelper, Notification, KEVOREE_REGISTRY_URL) {
+		var KEY_REG_LIBS = 'registry_libraries',
+			  KEY_REG_MODEL = 'registry_model',
+        KEY_REG_UPDATE = 'registry_update',
+			  KEY_REG = 'registry';
+		var model, tdefs, initiated, takeFromStorage, listener;
+		var url = storage.get(KEY_REG, KEVOREE_REGISTRY_URL);
 
-        function clean() {
-            model = null;
-            tdefs = {
-                groups: {},
-                nodes: {},
-                channels: {},
-                components: {},
-                getAll: function() {
-                    return [].concat(tdefs.groups)
-                        .concat(tdefs.nodes)
-                        .concat(tdefs.channels)
-                        .concat(tdefs.components);
-                }
-            };
-            initiated = false;
-            takeFromStorage = false;
-        }
+		function clean() {
+			model = null;
+			tdefs = {
+				groups: {},
+				nodes: {},
+				channels: {},
+				components: {},
+				getAll: function() {
+					return [].concat(tdefs.groups)
+						.concat(tdefs.nodes)
+						.concat(tdefs.channels)
+						.concat(tdefs.components);
+				}
+			};
+			initiated = false;
+			takeFromStorage = false;
+		}
 
-        clean();
+		/**
+		 * Returns a Promise with the Registry root model
+		 * @returns {*}
+		 */
+		function getAll() {
+			return $q(function(resolve, reject) {
+				if (model) {
+					resolve(model);
+				} else {
+					$http({
+							method: 'GET',
+							url: url,
+							headers: {
+								'Accept': 'application/json'
+							}
+						})
+						.then(function(res) {
+							try {
+								if (res.data.generated_KMF_ID !== storage.get(KEY_REG_UPDATE)) {
+									var modelStr = JSON.stringify(res.data);
+									takeFromStorage = false;
+									storage.set(KEY_REG_MODEL, modelStr);
+									var loader = kFactory.createJSONLoader();
+									model = loader.loadModelFromString(modelStr).get(0);
+									storage.set(KEY_REG_UPDATE, res.data.generated_KMF_ID);
+									resolve(model);
+								} else {
+									takeFromStorage = true;
+									resolve();
+								}
+							} catch (err) {
+								console.log('error', err.message);
+								reject(new Error('Unable to load Kevoree Registry model'));
+							}
+						})
+						.catch(function() {
+							clean();
+							storage.remove(KEY_REG_LIBS);
+							storage.remove(KEY_REG_MODEL);
+							storage.remove(KEY_REG_UPDATE);
+							reject(new Error('Unable to retrieve content from Kevoree Registry (' + url + ')'));
+						});
+				}
+			});
+		}
 
-        function init() {
-            if (initiated) {
-                return $q(function(resolve) {
-                    resolve(tdefs);
-                });
-            } else {
-                return getAll()
-                    .then(function(model) {
-                        if (takeFromStorage) {
-                            tdefs = storage.get(KEY_REG_LIBS);
-                            tdefs.getAll = function() {
-                                return [].concat(tdefs.groups)
-                                    .concat(tdefs.nodes)
-                                    .concat(tdefs.channels)
-                                    .concat(tdefs.components);
-                            };
-                        } else {
-                            // first init the different TypeDefinition items
-                            model.select('**/typeDefinitions[*]').array.forEach(function(tdef) {
-                                var pkgFqn = kModelHelper.genPkgName(tdef.eContainer()),
-                                    key = btoa(pkgFqn + '_' + tdef.name);
-                                tdefs[kModelHelper.getTypeDefinitionType(tdef) + 's'][key] = {
-                                    package: pkgFqn,
-                                    name: tdef.name,
-                                    platforms: [],
-                                    versions: [],
-                                    selected: false,
-                                    uiOpen: true
-                                };
-                            });
+		function init() {
+			if (initiated) {
+				return $q(function(resolve) {
+					resolve(tdefs);
+				});
+			} else {
+				return getAll()
+					.then(function(model) {
+						if (takeFromStorage) {
+							tdefs = storage.get(KEY_REG_LIBS);
+							tdefs.getAll = function() {
+								return [].concat(tdefs.groups)
+									.concat(tdefs.nodes)
+									.concat(tdefs.channels)
+									.concat(tdefs.components);
+							};
+						} else {
+							// first init the different TypeDefinition items
+							model.select('**/typeDefinitions[*]').array.forEach(function(tdef) {
+								var pkgFqn = kModelHelper.genPkgName(tdef.eContainer()),
+									key = btoa(pkgFqn + '_' + tdef.name),
+									descMeta = tdef.findMetaDataByID('description'),
+									desc = descMeta ? descMeta.value : null;
 
-                            // then do some process over it
-                            ['groups', 'nodes', 'components', 'channels'].forEach(function(type) {
-                                tdefs[type] = Object.keys(tdefs[type]).map(function(key) {
-                                    var siblings = model.select(kModelHelper.pkgFqnToPath(tdefs[type][key].package) + '/typeDefinitions[name=' + tdefs[type][key].name + ']'),
-                                        release = kModelHelper.getLatestRelease(siblings.array),
-                                        snapshot = kModelHelper.getLatestSnapshot(siblings.array);
+								tdefs[kModelHelper.getTypeDefinitionType(tdef) + 's'][key] = {
+									package: pkgFqn,
+									name: tdef.name,
+									platforms: [],
+									versions: [],
+									selected: false,
+									uiOpen: true,
+									description: desc
+								};
+							});
 
-                                    tdefs[type][key].release = release ? release.version : null;
-                                    tdefs[type][key].snapshot = snapshot ? snapshot.version : null;
+							// then do some process over it
+							['groups', 'nodes', 'components', 'channels'].forEach(function(type) {
+								tdefs[type] = Object.keys(tdefs[type]).map(function(key) {
+									var siblings = model.select(kModelHelper.pkgFqnToPath(tdefs[type][key].package) + '/typeDefinitions[name=' + tdefs[type][key].name + ']'),
+										release = kModelHelper.getLatestRelease(siblings.array),
+										snapshot = kModelHelper.getLatestSnapshot(siblings.array);
 
-                                    if (tdefs[type][key].release) {
-                                        tdefs[type][key].version = release.version;
-                                    } else if (tdefs[type][key].snapshot) {
-                                        tdefs[type][key].version = snapshot.version;
-                                    } else {
-                                        tdefs[type][key].version = null;
-                                    }
+									tdefs[type][key].release = release ? release.version : null;
+									tdefs[type][key].snapshot = snapshot ? snapshot.version : null;
 
-                                    if (release) {
-                                        tdefs[type][key].platforms = kModelHelper.getPlatforms(release);
-                                    } else if (snapshot) {
-                                        tdefs[type][key].platforms = kModelHelper.getPlatforms(snapshot);
-                                    }
+									if (tdefs[type][key].release) {
+										tdefs[type][key].version = release.version;
+									} else if (tdefs[type][key].snapshot) {
+										tdefs[type][key].version = snapshot.version;
+									} else {
+										tdefs[type][key].version = null;
+									}
 
-                                    siblings.array.forEach(function(tdef) {
-                                        if (tdefs[type][key].versions.indexOf(tdef.version) === -1) {
-                                            tdefs[type][key].versions.push(tdef.version);
-                                        }
-                                    });
+									if (release) {
+										tdefs[type][key].platforms = kModelHelper.getPlatforms(release);
+									} else if (snapshot) {
+										tdefs[type][key].platforms = kModelHelper.getPlatforms(snapshot);
+									}
 
-                                    return tdefs[type][key];
-                                });
-                            });
-                            storage.set(KEY_REG_LIBS, tdefs);
-                        }
+									siblings.array.forEach(function(tdef) {
+										if (tdefs[type][key].versions.indexOf(tdef.version) === -1) {
+											tdefs[type][key].versions.push(tdef.version);
+										}
+									});
 
-                        initiated = true;
-                        return tdefs;
-                    });
-            }
-        }
+									return tdefs[type][key];
+								});
+							});
+							storage.set(KEY_REG_LIBS, tdefs);
+						}
 
-        /**
-         * Returns a Promise with the Registry root model
-         * @returns {*}
-         */
-        function getAll() {
-            return $q(function(resolve, reject) {
-                if (model) {
-                    resolve(model);
-                } else {
-                    $http({
-                            method: 'GET',
-                            url: url,
-                            headers: {
-                                'Accept': 'application/json'
-                            }
-                        })
-                        .then(function(res) {
-                            try {
-                                var modelStr = JSON.stringify(res.data);
-                                if (modelStr === storage.get(KEY_REG_MODEL)) {
-                                    takeFromStorage = true;
-                                    resolve();
-                                } else {
-                                    takeFromStorage = false;
-                                    storage.set(KEY_REG_MODEL, modelStr);
-                                    var loader = kFactory.createJSONLoader();
-                                    model = loader.loadModelFromString(modelStr).get(0);
-                                    resolve(model);
-                                }
-                            } catch (err) {
-                                console.log('error', err.message);
-                                reject(new Error('Unable to load Kevoree Registry model'));
-                            }
-                        })
-                        .catch(function() {
-                            clean();
-                            storage.remove(KEY_REG_LIBS);
-                            storage.remove(KEY_REG_MODEL);
-                            reject(new Error('Unable to retrieve content from Kevoree Registry (' + url + ')'));
-                        });
-                }
-            });
-        }
+						initiated = true;
+						return tdefs;
+					});
+			}
+		}
 
-        /**
-         * Returns a Promise with the requested model if any
-         * @param path a Kevoree Modeling Framework path (eg. /foo[*]/bar[*])
-         * @returns {*}
-         */
-        function get(path) {
-            return $q(function(resolve, reject) {
-                $http({
-                        method: 'POST',
-                        url: url,
-                        headers: {
-                            'Content-Type': 'text/plain'
-                        },
-                        data: [path]
-                    })
-                    .then(function(res) {
-                        try {
-                            var modelStr = JSON.stringify(res.data);
-                            var loader = kFactory.createJSONLoader();
-                            resolve(loader.loadModelFromString(modelStr).get(0));
-                        } catch (err) {
-                            console.log('error', err.message);
-                            reject(new Error('Unable to load Kevoree Registry model for "' + path + '"'));
-                        }
-                    })
-                    .catch(function() {
-                        clean();
-                        storage.remove(KEY_REG_LIBS);
-                        storage.remove(KEY_REG_MODEL);
-                        reject(new Error('Unable to retrieve content from Kevoree Registry (' + url + ')'));
-                    });
-            });
-        }
+		/**
+		 * Returns a Promise with the requested model if any
+		 * @param path a Kevoree Modeling Framework path (eg. /foo[*]/bar[*])
+		 * @returns {*}
+		 */
+		function get(path) {
+			return $q(function(resolve, reject) {
+				$http({
+						method: 'POST',
+						url: url,
+						headers: {
+							'Content-Type': 'text/plain'
+						},
+						data: [path]
+					})
+					.then(function(res) {
+						try {
+							var modelStr = JSON.stringify(res.data);
+							var loader = kFactory.createJSONLoader();
+							resolve(loader.loadModelFromString(modelStr).get(0));
+						} catch (err) {
+							console.log('error', err.message);
+							reject(new Error('Unable to load Kevoree Registry model for "' + path + '"'));
+						}
+					})
+					.catch(function() {
+						clean();
+						storage.remove(KEY_REG_LIBS);
+						storage.remove(KEY_REG_MODEL);
+						storage.remove(KEY_REG_UPDATE);
+						reject(new Error('Unable to retrieve content from Kevoree Registry (' + url + ')'));
+					});
+			});
+		}
 
-        /**
-         * Saves current tdefs state to storage
-         */
-        function save() {
-            storage.set(KEY_REG_LIBS, tdefs);
-        }
+		/**
+		 * Saves current tdefs state to storage
+		 */
+		function save() {
+			storage.set(KEY_REG_LIBS, tdefs);
+		}
 
-        function isInit() {
-            var model = storage.get(KEY_REG_MODEL);
-            return model ? model.length > 0 : false;
-        }
+		function isInit() {
+			var model = storage.get(KEY_REG_MODEL);
+			return model ? model.length > 0 : false;
+		}
 
-        function clearCache() {
-            clean();
-            storage.remove(KEY_REG_LIBS);
-            storage.remove(KEY_REG_MODEL);
-            Notification.success({
-                title: 'Registry Libraries cache',
-                message: 'Cleared successfully',
-                delay: 3000
-            });
-        }
+		function clearCache() {
+			clean();
+			storage.remove(KEY_REG_LIBS);
+			storage.remove(KEY_REG_MODEL);
+			storage.remove(KEY_REG_UPDATE);
+			Notification.success({
+				title: 'Registry Libraries cache',
+				message: 'Cleared successfully',
+				delay: 3000
+			});
+		}
 
-        return {
-            init: init,
-            get: get,
-            save: save,
-            isInit: isInit,
-            clearCache: clearCache,
-            getUrl: function() {
-                return url;
-            },
-            setUrl: function(newUrl) {
-                url = newUrl;
-                storage.set(KEY_REG, url);
-                clearCache();
-                if (listener && typeof listener === 'function') {
-                    listener();
-                }
-            },
-            onUrlChange: function (handler) {
-                listener = handler;
-            }
-        };
-    });
+		// generate a brand new tdefs list
+		clean();
+
+		return {
+			init: init,
+			get: get,
+			save: save,
+			isInit: isInit,
+			clearCache: clearCache,
+			getUrl: function() {
+				return url;
+			},
+			setUrl: function(newUrl) {
+				url = newUrl;
+				storage.set(KEY_REG, url);
+				clearCache();
+				if (listener && typeof listener === 'function') {
+					listener();
+				}
+			},
+			onUrlChange: function(handler) {
+				listener = handler;
+			}
+		};
+	});
