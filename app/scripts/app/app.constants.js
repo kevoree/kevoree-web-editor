db1acc5e9d8077aeaa28b1cabd38c0f154be43d1
Maxime Tricoire <max.tricoire@gmail.com>
2016-06-01 17:19:09 +0200

    v5.12.1: selection by type only selects non-filtered items

--- app/scripts/app/app.constants.js
+++ app/scripts/app/app.constants.js
@@ -3,7 +3,7 @@ angular.module('editorApp')
 
 .constant('APP_NAME', 'Kevoree Web Editor')
 
-.constant('VERSION', '5.12.0')
+.constant('VERSION', '5.12.1')
 
 .constant('NPM_REGISTRY_URL', 'http://registry.npmjs.org/{name}/-/{name}-{version}.tgz')
 
